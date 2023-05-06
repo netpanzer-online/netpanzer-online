04dd0cbc15e4df16eaa4fad30c69c2ae15060762
winrid <winrid@gmail.com>
2023-05-06 00:55:53 -0700

    fixing compile err

--- server/projectiles.ts
+++ server/projectiles.ts
@@ -1,7 +1,7 @@
-import {OpType, Projectile} from "core";
-import {ServerState} from "./index";
+import {Projectile} from "core";
+import {ServerState} from "./map";
 
 export function tick_projectile(state: ServerState, projectile: Projectile) {
     // example - todo - will we splice from array now or periodically GC projectiles??
-    state.pending_ops.push([OpType.ProjectileLanded, projectile.id]);
+    // state.pending_ops.push([OpType.ProjectileLanded, projectile.id]);
 }
